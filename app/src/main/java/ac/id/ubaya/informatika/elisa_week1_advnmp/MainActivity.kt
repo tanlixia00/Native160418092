48085a47ce7bd6f344cd86e53e5ebe2717594cb1
tanlixia00 <62798315+tanlixia00@users.noreply.github.com>
2021-02-10 17:47:01 +0700

    initial

---
+++ app/src/main/java/ac/id/ubaya/informatika/elisa_week1_advnmp/MainActivity.kt
@@ -0,0 +1,11 @@
+package ac.id.ubaya.informatika.elisa_week1_advnmp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
